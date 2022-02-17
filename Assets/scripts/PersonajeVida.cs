af7c583b7cd36e832bbd9fbd9d8ca1cb7712ba64
Juan Moreno <morjua19@gmail.com>
2022-02-16 21:26:46 -0300

    [UPD]

---
+++ Assets/scripts/PersonajeVida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class PersonajeVida: VidaBase
+{
+    public bool puedeSerCurado => Salud < saludMax;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            RecibirDaño(10);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestaurarSalud(10);
+        }
+    }
+
+    public void RestaurarSalud(float cantidad)
+    {
+        if (puedeSerCurado)
+        {
+            Salud += cantidad;
+            if (Salud > saludMax)
+            {
+                Salud = saludMax;
+            }
+            ActualizarBarraVida(Salud, saludMax);
+        }
+    }
+
+    protected override void ActualizarBarraVida(float vidaActual, float vidaMax)
+    {
+        
+    }
+}
